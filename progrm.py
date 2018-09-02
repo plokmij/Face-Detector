0c699b8d09e29a81e5148822eabc5c1add24a691
Samfan <065samfan@gmail.com>
2018-09-03 02:54:25 +0530

    code is converted a preprocess function

--- progrm.py
+++ progrm.py
@@ -1,16 +1,3 @@
-"""
-import cv2
-import numpy as np
-from matplotlib import pyplot as plt
-
-img = cv2.imread('aa.jpeg', 0 )
-gimg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-blur = cv2.GaussianBlur(img,(5,5),0)
-th3 = cv2.adaptiveThreshold(gimg,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
-
-
-
-"""
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
@@ -23,7 +10,8 @@ def preprocess(img)
 
 	edges = cv2.Canny( th3, 100, 200)
 	backdel = edges - gray_image
+	fileName = img + "prepro"
+	cv2.imwrite('fileName', backdel )
 
-	cv2.imshow('image', backdel ) 
-	cv2.waitKey(0)                 # Waits forever for user to press any key
-	cv2.destroyAllWindows() 
+
+preprocess('aa.jpeg')
