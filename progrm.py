8b7479b511901cd93d3c89dea44f1a6e3f6e2e71
Samfan <065samfan@gmail.com>
2018-09-02 17:17:46 +0530

    Thersholds input image

--- progrm.py
+++ progrm.py
@@ -8,22 +8,19 @@ gimg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 blur = cv2.GaussianBlur(img,(5,5),0)
 th3 = cv2.adaptiveThreshold(gimg,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
 
-plt.subplot(121),plt.imshow(gimg),plt.title('Original')
-plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(gth3),plt.title('Blurred')
-plt.xticks([]), plt.yticks([])
-plt.show()
+
 
 """
 import cv2
+import numpy as np
 from matplotlib import pyplot as plt
 
 image = cv2.imread('aa.jpeg')
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-blur = cv2.GaussianBlur(gray_image,(5,5),0)
-th3 = cv2.adaptiveThreshold(gray_image,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
+blur = cv2.GaussianBlur(gray_image,(15,15),0)
+th3 = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
 
-
-cv2.imshow('gray_image',gray_image) 
+cv2.imshow('gray_image', th3 ) 
+#cv2.imshow('gray_image',gray_image) 
 cv2.waitKey(0)                 # Waits forever for user to press any key
 cv2.destroyAllWindows() 
