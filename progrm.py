faa5ec205e92aaad92b9f3274279906cd905b94a
Samfan <065samfan@gmail.com>
2018-09-02 17:06:19 +0530

    Blurs th image

--- progrm.py
+++ progrm.py
@@ -15,12 +15,15 @@ plt.xticks([]), plt.yticks([])
 plt.show()
 
 """
-
 import cv2
+from matplotlib import pyplot as plt
+
 image = cv2.imread('aa.jpeg')
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-cv2.imwrite('gray_image.png',gray_image)
-cv2.imshow('color_image',image)
+blur = cv2.GaussianBlur(gray_image,(5,5),0)
+th3 = cv2.adaptiveThreshold(gray_image,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
+
+
 cv2.imshow('gray_image',gray_image) 
 cv2.waitKey(0)                 # Waits forever for user to press any key
 cv2.destroyAllWindows() 
